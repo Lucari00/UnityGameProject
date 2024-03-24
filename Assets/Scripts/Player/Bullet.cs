fb21632a2f05c86b6bfe089be9035934b8590ba4
Luca <luca.palaysi@gmail.com>
2024-03-24 18:53:48 +0100

    Trying collision with bullet

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
+    public GameObject fxSmoke;
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x > 300 || transform.position.x < -300 || transform.position.z > 300 || transform.position.z < -300) {
+            gameObject.SetActive(false);
+        }
         transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other);
+        if (other.CompareTag("Enemy")) {
+            Debug.Log("kill enemy");
+        } else if (other.CompareTag("Wall")) {
+            Debug.Log("here");
+        }
+        gameObject.SetActive(false);
+        Instantiate(fxSmoke, transform);
     }
 }
